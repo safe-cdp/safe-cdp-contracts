88b1184b83a0dadf201f40bcee3b1c698fd73d47
Derek Chiang <me@derekchiang.com>
2019-02-24 19:37:44 -0800

    Clean up the SafeCDP interface

--- contracts/SafeCDP.sol
+++ contracts/SafeCDP.sol
@@ -4,33 +4,40 @@ import "ds-thing/thing.sol";
 
 contract SafeCDP is DSThing {
 
-    address actualOwner;
+    address cdpOwner;
+    address tub;
+    bytes32 cup;
 
-    modifier onlyActualOwner() {
-        require(msg.sender == actualOwner, "not actual owner");
+    modifier onlyCDPOwner() {
+        require(msg.sender == cdpOwner, "Not CDP owner.");
         _;
     }
 
+    // Making sure that this contract can only be used if the CDP has
+    // actually been given to this contract.
+    modifier ifCDPGiven() {
+        require(this == tub.lad(cup), "This contract doesn't own the CDP.");
+        _;
+    }
 
-    // These functions are the tub functions that can only be invoked by
-    // the CDP owner.  Since this contract will technically be the CDP owner,
-    // we need to expose these functions so the actual owner can still use
-    // them.
-    function give(bytes32 cup, address guy) public note onlyActualOwner { }
-    function free(bytes32 cup, uint wad) public note onlyActualOwner { }
-    function draw(bytes32 cup, uint wad) public note onlyActualOwner { }
-    function shut(bytes32 cup) public note onlyActualOwner { }
+    // Certain functions from the SaiProxy are priviledged, in that they can
+    // only be invoked by the owner.
+    // Since this contract is technically the owner of CDP, we need to expose
+    // those functions here so clients (e.g. the CDP portal) can work with
+    // minimal modifications.
+    // TODO: implement those functions
 
     // When the collateralization ratio is lower than the margin call
     // threshold, this function may be invoked by a keeper to wipe enough
     // debt to raise the threshold to the target.
-    function marginCall() public note { }
+    function marginCall() public note ifCDPGiven { }
     
     // When invoked, the msg.sender will pay all the keepers that have covered
     // debt for this CDP.
-    function respondToMarginCall() public note { }
+    function respondToMarginCall() public note ifCDPGiven { }
 
     // A keeper may call this function to withdraw part of the collateral if
     // the CDP owner fails to respond to a margin call.
-    function withdrawCollateral(uint wad) public note { }
+    function withdrawCollateral(uint _wad) public note ifCDPGiven { }
+
 }
